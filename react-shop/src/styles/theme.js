44827794e35579534cb5600008514493542beb52
chaeeunj <official2930@naver.com>
2023-07-12 03:16:56 +0900

    공통 스타일 관리를 위한 theme.js 파일 생성

---
+++ react-shop/src/styles/theme.js
@@ -0,0 +1,22 @@
+const lightColor = {
+  navBack: '#fff',
+  navText: '#374151',
+  commonText: '#1F2937',
+  input: '#D1D5DB',
+  itemDescBack: '#F3F4F6',
+};
+
+const darkColor = {
+  navBack: '#191D24',
+  navText: '#fff',
+  commonText: '#A6ADBB',
+  input: '#4B5563',
+  itemDescBack: '#374151',
+};
+
+const theme = {
+  lightColor,
+  darkColor,
+};
+
+export default theme;
